a2e8355cff243a74653b92de971d74edd888e5ed
victoria <victoriasegovia001.alu@stemgranada.com>
2025-09-25 21:23:10 +0200

    feat: controller created

---
+++ Counting Tree/backend/src/main/java/com/countingTree/Counting/Tree/App/controller/CommentController.java
@@ -0,0 +1,44 @@
+package com.countingTree.Counting.Tree.App.controller;
+
+import com.countingTree.Counting.Tree.App.model.Comment;
+import com.countingTree.Counting.Tree.App.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Comment> getCommentById(@PathVariable Long id) {
+        return ResponseEntity.ok(commentService.getCommentById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addComment(@RequestBody Comment comment) {
+        commentService.addComment(comment);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateComment(@PathVariable Long id, @RequestBody Comment comment) {
+        commentService.updateComment(id, comment);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+        commentService.deleteComment(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Comment>> getAllComments() {
+        return ResponseEntity.ok(commentService.getAllComments());
+    }
+}
